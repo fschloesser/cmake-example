741416ee3d270f7f4aa23cd2df1c5364ea306c0a
Franziska Schloesser <schloesser@zib.de>
2021-08-26 12:33:07 +0200

    add minimal example

---
+++ c++/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello world\n";
+  int a = 0;
+  int b = 1;
+  if (a == b or a-1 == 1) {
+    std::cout << "a and b are close\n";
+  }
+  return 0;
+}
+
